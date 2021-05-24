463001fdee690c0c7a0897a94e43b5a5534c8123
Alexander <sphc@yandex.ru>
2021-05-24 14:18:53 +0300

    added all modules

--- core/src/main/kotlin/com/rasalexman/sresult/core/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.rasalexman.sresult.core
-
-class MyClass {
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.buildFileName = "build.gradle.kts"
 rootProject.name = "SResultExample"
 include(":app")
-include(":core")
 include(":sresult")
+include(":sresultpresentation")
