ed3b9ad79b0de9bbefb7703abb3e22016f760932
Alexander <sphc@yandex.ru>
2021-06-03 16:20:31 +0300

    update version to 1.1.3 update type alisases for Flow<ISResult<T>>

--- sresult/src/main/kotlin/com/rasalexman/sresult/common/typealiases/TypeAliases.kt
+++ sresult/src/main/kotlin/com/rasalexman/sresult/common/typealiases/TypeAliases.kt
@@ -4,6 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.rasalexman.sresult.data.dto.ISResult
 import com.rasalexman.sresult.data.dto.SResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 
 typealias UnitHandler = () -> Unit
 typealias InHandler<T> = (T) -> Unit
@@ -20,6 +24,15 @@ typealias SInSameOutHandler<T> = suspend (T) -> T
 typealias SInOutHandler<T, R> = suspend (T) -> R
 typealias SDoubleInOutHandler<T, R, P> = suspend (T, R) -> P
 typealias SParentInOutHandler<P, T, R> = suspend P.(T) -> R
+
+typealias FlowResult<T> = Flow<SResult<T>>
+typealias FlowList<T> = Flow<List<T>>
+typealias FlowResultList<T> = Flow<SResult<List<T>>>
+typealias SharedFlowList<T> = SharedFlow<List<T>>
+typealias SharedFlowResultList<T> = SharedFlow<SResult<List<T>>>
+typealias MutableSharedFlowList<T> = MutableSharedFlow<List<T>>
+typealias MutableStateFlowList<T> = MutableStateFlow<List<T>>
+typealias MutableStateFlowResultList<T> = MutableStateFlow<SResult<List<T>>>
 
 typealias AnyResult = ISResult<Any>
 typealias AnyResultLiveData = LiveData<AnyResult>
