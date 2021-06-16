c3805006bacd73d8832a03d68c03aff5b6fe99be
Alexander <sphc@yandex.ru>
2021-06-16 13:12:14 +0300

    update version to 1.2.11 - revert EditTextDataBindingAdapters.kt

--- buildSrc/src/main/kotlin/config/Builds.kt
+++ buildSrc/src/main/kotlin/config/Builds.kt
@@ -17,7 +17,7 @@ object Builds {
     }
 
     object SResult {
-        const val VERSION_CODE = 102010
-        const val VERSION_NAME = "1.2.10"
+        const val VERSION_CODE = 102011
+        const val VERSION_NAME = "1.2.11"
     }
 }
