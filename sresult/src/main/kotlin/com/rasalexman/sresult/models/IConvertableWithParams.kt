58883051f18db3a3cd28bb055cbb261bf53c76ac
Misha Trishin <mihail.trishin@krit.pro>
2021-07-13 17:17:55 +0300

    Add function convertAsWithParams and convert extension in IConvertableWithParams.kt

--- sresult/src/main/kotlin/com/rasalexman/sresult/models/IConvertableWithParams.kt
+++ sresult/src/main/kotlin/com/rasalexman/sresult/models/IConvertableWithParams.kt
@@ -5,7 +5,14 @@ import kotlin.reflect.KClass
 interface IConvertableWithParams<out T, in R : Any?> : IConvertable {
     fun convertTo(param: R): T?
 
-    override fun <I : Any> convertAs(clazz: KClass<I>): I? {
-        return null
+    @Suppress("UNCHECKED_CAST")
+    fun <I : Any> convertAsWithParams(clazz: KClass<I>, params: R): I? {
+        return convertTo(params) as? I
     }
+
+    override fun <I : Any> convertAs(clazz: KClass<I>): I? = null
+}
+
+inline fun <reified To : Any, O: Any> IConvertableWithParams<To, O>.convert(params: O): To? {
+    return convertAsWithParams(To::class, params)
 }
