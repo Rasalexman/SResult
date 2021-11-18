dce9428c9083f57893a60460284242994e88a68d
Alexander <sphc@yandex.ru>
2021-11-18 15:03:39 +0300

    release 1.3.31

--- buildSrc/src/main/kotlin/config/Builds.kt
+++ buildSrc/src/main/kotlin/config/Builds.kt
@@ -16,7 +16,7 @@ object Builds {
     }
 
     object SResult {
-        const val VERSION_CODE = 103030
-        const val VERSION_NAME = "1.3.30"
+        const val VERSION_CODE = 103031
+        const val VERSION_NAME = "1.3.31"
     }
 }
