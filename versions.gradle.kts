b2d3da6e1bc8214aae37738c49db6f3273ba81b1
Alexander <sphc@yandex.ru>
2022-03-15 17:33:36 +0300

    release version 1.3.43 update libs version and downgrade minSdk to 18

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -1,36 +1,36 @@
 //------ APP VERSION
-extra["appVersion"] = "1.3.42"
+extra["appVersion"] = "1.3.43"
 extra["mainGroupName"] = "com.rasalexman.sresult"
 extra["supportGroupName"] = "com.rasalexman.sresultpresentation"
 
 //------ CONFIG DATA
-extra["minSdkVersion"] = 19
+extra["minSdkVersion"] = 18
 extra["buildSdkVersion"] = 31
 extra["toolsVersion"] = "31.0.0"
 extra["apiVersion"] = "1.6"
 extra["jvmVersion"] = "11"
-extra["agpVersion"] = "7.0.4"//"7.1.0" // use agp 7.1 jnly with stable android studio
+extra["agpVersion"] = "7.1.2"
 extra["kotlinVersion"] = "1.6.10"
 extra["jitpackPath"] = "https://jitpack.io"
 extra["codePath"] = "src/main/kotlin"
 extra["resPath"] = "src/main/res"
 
 //------- LIBS VERSIONS
-val navigation = "2.4.1"//"2.5.0-alpha01"
+val navigation = "2.5.0-alpha03"
 val leakcanary = "2.8.1"
 val coroutines = "1.6.0"
 val core: String = "1.7.0"
 val constraintLayout = "2.1.3"
-val material = "1.6.0-alpha02"
+val material = "1.6.0-alpha03"
 val viewPager2 = "1.1.0-beta01"
 val recyclerView = "1.3.0-alpha01"
-val lifecycle = "2.4.1"
-val fragment: String = "1.4.1"
+val lifecycle = "2.5.0-alpha04"
+val fragment: String = "1.5.0-alpha03"
 val paging: String = "3.1.0"
 val timber = "5.0.1"
-val kodi = "1.5.23"
+val kodi = "1.6.2"
 val coroutinesManager = "1.4.0"
-val easyBinding = "1.2.5"
+val easyBinding = "1.2.6"
 extra["navigation"] = navigation
 
 //------- Libs path
