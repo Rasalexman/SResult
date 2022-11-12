5d344e9e152264dab3897fa727650efadc3dec45
Alexander <sphc@yandex.ru>
2022-11-12 21:19:24 +0300

    Kmm specific platform check

--- sresultcore/build.gradle.kts
+++ sresultcore/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.os.OperatingSystem;
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -23,20 +25,21 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 }
 
 kotlin {
+    val opSystem = OperatingSystem.current()
+
     android {
         publishLibraryVariantsGroupedByFlavor = true
         publishLibraryVariants("release", "debug")
     }
-    jvm()
-    ios()
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "sresultcore"
+    if(opSystem.isMacOsX) {
+        listOf(
+            iosX64(),
+            iosArm64(),
+            iosSimulatorArm64()
+        ).forEach {
+            it.binaries.framework {
+                baseName = "sresultcore"
+            }
         }
     }
 
@@ -52,25 +55,17 @@ kotlin {
             dependsOn(commonMain)
         }
 
-        val iosX64Main by getting
-        val iosArm64Main by getting
-//        val iosSimulatorArm64Main by getting
+        if(opSystem.isMacOsX) {
+            val iosX64Main by getting
+            val iosArm64Main by getting
+            val iosSimulatorArm64Main by getting
 
-        val iosMain by getting {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            //iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        //val iosSimulatorArm64Test by getting
-
-        val iosTest by getting {
-            //dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            //iosSimulatorArm64Test.dependsOn(this)
+            val iosMain by creating {
+                dependsOn(commonMain)
+                iosX64Main.dependsOn(this)
+                iosArm64Main.dependsOn(this)
+                iosSimulatorArm64Main.dependsOn(this)
+            }
         }
     }
 }
